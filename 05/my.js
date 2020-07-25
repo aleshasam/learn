1ad36bbca5145b0f4f60972cba4ce25010bc6415
alexey <aleshasam@mail.ru>
2020-07-25 20:21:29 +0700

    first Commit

---
+++ 05/my.js
@@ -0,0 +1,16 @@
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj.a);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[0]);
---
+++ 07/my.js
@@ -0,0 +1,13 @@
+//alert('Hello');
+
+//const result = confirm('Are you here?')
+//console.log(result)
+
+//const answer = prompt('Вам есть 18?', '18');
+//console.log(answer);
+
+const answers = [];
+
+//answers[0] = prompt('Как вас зовут?', '');
+//answers[1] = prompt('Какая у вас фамилия?', '');
+//answers[2] = prompt('сколько вам лет?', '');
