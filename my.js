6bf4aafaf0634faeb32ca61a490f3aa87add0249
alexey <aleshasam@mail.ru>
2020-07-29 19:16:49 +0700

    learn

---
+++ my.js
@@ -0,0 +1,16 @@
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('Неверно');
+        break;
+    case 100:
+        console.log('Неверно');
+        break;
+    case 50:
+        console.log('Верно');
+        break;
+    default:
+        console.log('Не получилось');
+        break;
+}
