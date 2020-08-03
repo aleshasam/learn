76b14056ad1d4abf9c4b93b08bb5a28554ae87e5
alexey <aleshasam@mail.ru>
2020-08-03 15:00:45 +0700

    udemy

---
+++ 28 getting element/js/script.js
@@ -0,0 +1,14 @@
+const box = document.getElementById('box');
+console.log(box);
+
+const btns = document.getElementsByTagName('button'); //получаю псевдомассив или эелемент из него
+console.log(btns[1]);
+
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
+
+const hearts = document.querySelectorAll('.heart');
+
+hearts.forEach(item => {
+    console.log(item);
+});
