f7adc843291b8aaff43c46f628a1a3bbce62a5f4
alexey <aleshasam@mail.ru>
2020-08-25 17:15:58 +0700

    udemy

---
+++ 47 classes/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    calcArea() {
+        return this.height * this.width;
+    }
+}
+
+class ColoredRectangleWithText extends Rectangle {
+    constructor(height, width, text, bgColor) {
+        super(height, width); //повторяет все ,что есть в родителе
+        this.text = text;
+        this.bgColor = bgColor;
+    }
+
+    showMyProps() {
+        console.log(`Текст:${this.text}, Цвет: ${this.bgColor}`);
+    }
+}
+
+const div = new ColoredRectangleWithText(25, 10, 'Hello world!', 'red');
+div.showMyProps();
+console.log(div.calcArea());
+// const square = new Rectangle(10, 10);
+// const long = new Rectangle(20, 100);
+
+// console.log(square.calcArea());
+// console.log(long.calcArea());
